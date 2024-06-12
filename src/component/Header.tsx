c8612bb1d82e2a60ef8d35a7dff487a16087fe6b
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-06-12 20:53:04 +0530

    header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,6 +3,7 @@ import Style from "./header.module.scss";
 import HDImg from "../img/nikhil-logo-removebg-preview.png";
 import { useAppDispatch, useAppSelector } from "../redux/hook";
 import { fetchHeader } from "../redux/Slice";
+import Link from "next/link";
 
 const Header = () => {
   const dispatch = useAppDispatch();
@@ -22,7 +23,7 @@ const Header = () => {
               data.headerData.Header.map((item) => {
                 return (
                   <li>
-                    <a href={item.url}>{item.title}</a>
+                    <Link href={item.url}>{item.title}</Link>
                     <div className={Style.drop}>
                       {item.subMenu &&
                         item.subMenu.map((item) => {
