2ddbda6c53c9aa22a20d68a791450d6860bdf793
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-06-12 20:55:42 +0530

    router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-// import SliceData from "./redux/SliceData";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import App from "./App";
@@ -15,7 +14,6 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Header />
-      {/* <SliceData /> */}
       <App />
       <Footer />
     </Provider>
