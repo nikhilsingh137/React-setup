8b9413111caeb0e4380d0b006d4b20b890b21456
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-06-14 03:50:20 +0530

    component

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,7 +3,6 @@ import Style from "./header.module.scss";
 import HDImg from "../img/nikhil-logo-removebg-preview.png";
 import { useAppDispatch, useAppSelector } from "../redux/hook";
 import { fetchHeader } from "../redux/Slice";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   const dispatch = useAppDispatch();
@@ -24,7 +23,7 @@ const Header = () => {
               data.headerData.Header.map((item) => {
                 return (
                   <li key={item.id}>
-                    <Link to={item.url}>{item.title}</Link>
+                    <a href={item.url}>{item.title}</a>
                     <div className={Style.drop}>
                       {item.subMenu &&
                         item.subMenu.map((item) => {
